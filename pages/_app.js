34c91f8afe41ad0bd215bc6e6f5fe98642e97e74
Yago Gonzalez <jagogc@gmail.com>
2021-04-28 23:01:44 +0100

    WIP: Add Header to index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>PLP test</title>
+        <meta name="description" content="PLP technical test" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
