a5fdf2576dee62be18bf62b56ffcc5fae4e66034
Yago Gonzalez <jagogc@gmail.com>
2021-04-29 00:06:22 +0100

    Fix Header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   const numberOfLikes = 0
   return (
     <header className="bg-white border-gray-400 border-solid shadow border-b-1">
-      <div className="container flex flex-row-reverse p-2">
+      <div className="container flex flex-row-reverse p-2 mx-auto">
         <button className="px-3 py-1 text-lg font-bold text-white bg-purple-800 border-2 border-purple-800 rounded hover:bg-purple-600">{`${numberOfLikes} 👍`}</button>
       </div>
     </header>
