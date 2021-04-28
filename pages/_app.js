e0d78dc746c798cd70d827b46b2798fd771286dd
Yago Gonzalez <jagogc@gmail.com>
2021-04-29 00:07:10 +0100

    WIP: Implement products query w/ ProductCard

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import '../styles/globals.css'
 
 import Head from 'next/head'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>PLP test</title>
         <meta name="description" content="PLP technical test" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   )
 }
 
