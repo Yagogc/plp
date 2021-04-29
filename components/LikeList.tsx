596901d1afcbae4cc29453a6dc7c1a5b3aee6add
Yago Gonzalez <jagogc@gmail.com>
2021-04-29 12:32:33 +0100

    Animate LikeList

--- components/LikeList.tsx
+++ components/LikeList.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion'
 import { useState } from 'react'
 
 import useLikeStore from '../state/client/likeStore'
@@ -21,25 +22,63 @@ const LikeList = () => {
           isLoading && 'grayscale opacity-50 cursor-not-allowed'
         }`}
       >{`${numberOfLikes} 👍`}</button>
-      {isLikeListOpen && !isError && (
-        <div className="absolute right-0 z-50 flex flex-col gap-1 p-1 mt-1 bg-white border-2 border-purple-800 border-solid rounded shadow-md top-full">
-          {data
-            ?.filter((prod) => likedProductsId.includes(prod.id))
-            .map((product) => (
-              <button
-                onClick={() => toggleLikedProductId(product.id)}
-                key={product.id}
-                className="flex items-center justify-between gap-4 px-2 py-1 transition-all rounded whitespace-nowrap hover:bg-gray-300"
-              >
-                <span>{product.name}</span>
+      <AnimatePresence>
+        {isLikeListOpen && !isError && (
+          <AnimateSharedLayout>
+            <motion.ul
+              layout
+              layoutId="like-list"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, x: 50 }}
+              transition={{ type: 'tween' }}
+              className="absolute right-0 z-50 flex flex-col p-1 mt-1 space-y-1 overflow-hidden bg-white border-2 border-purple-800 border-solid rounded shadow-md top-full"
+            >
+              <AnimatePresence>
+                {numberOfLikes === 0 ? (
+                  <motion.li
+                    layoutId="no-likes"
+                    layout
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ type: 'tween' }}
+                    className="px-2 py-1 whitespace-nowrap"
+                  >
+                    No Liked Products
+                  </motion.li>
+                ) : (
+                  data
+                    ?.filter((prod) => likedProductsId.includes(prod.id))
+                    .map((product) => (
+                      <motion.li
+                        key={product.id}
+                        layout
+                        layoutId={product.id}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: 50 }}
+                        transition={{ type: 'tween' }}
+                        className="w-full"
+                      >
+                        <button
+                          onClick={() => toggleLikedProductId(product.id)}
+                          className="flex items-center justify-between w-full h-8 px-2 py-1 space-x-4 transition-all rounded whitespace-nowrap hover:bg-gray-300"
+                        >
+                          <span>{product.name}</span>
 
-                <span role="img" aria-label="remove">
-                  ❌
-                </span>
-              </button>
-            ))}
-        </div>
-      )}
+                          <span role="img" aria-label="remove">
+                            ❌
+                          </span>
+                        </button>
+                      </motion.li>
+                    ))
+                )}
+              </AnimatePresence>
+            </motion.ul>
+          </AnimateSharedLayout>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
