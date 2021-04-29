0ea69ea552cfb40b2056ad4b617a4733e6a5b8fe
Yago Gonzalez <jagogc@gmail.com>
2021-04-29 11:17:21 +0100

    Remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { Header } from '../components/Header'
 import { ProductList } from '../components/ProductList'
-import { useProducts } from '../state/server/getProducts'
 
 const Home = () => {
   return (
