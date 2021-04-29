4ac3c96d60383e9d6285a0a7e9bad99d74e604c5
Yago Gonzalez <jagogc@gmail.com>
2021-04-29 11:15:09 +0100

    Update ProductCard with like logic

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
+import useLikeStore from '../state/client/likeStore'
 import { Product } from '../state/server/getProducts'
 
 const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
+  // Dirty override of the placeholder image to avoid HTTPS issues
+  const imageUrl = `https://loremflickr.com/320/240/product?random=${product.id}`
+  const likedProductsId = useLikeStore((state) => state.likedProductsId)
+  const toggleLikedProductId = useLikeStore(
+    (state) => state.toggleLikedProductId
+  )
+  const isLiked = likedProductsId.includes(product.id)
+  const handleClick = () => toggleLikedProductId(product.id)
+
   return (
-    <div className="flex flex-col w-full max-w-xs gap-2 p-4 bg-white rounded shadow">
+    <div className="flex flex-col w-full gap-2 p-4 bg-white rounded shadow">
       <div className="relative h-40 overflow-hidden rounded">
         <img
-          src={product.img}
+          src={imageUrl}
           alt={product.name}
-          className="object-cover min-h-full bg-gray-400"
+          className="object-cover object-center min-w-full min-h-full bg-gray-400"
         />
         {product.sold && (
           <>
@@ -19,7 +29,12 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
             </span>
           </>
         )}
-        <button className="absolute px-2 py-1 transition-all bg-gray-300 bg-opacity-50 border-2 border-gray-500 border-solid rounded top-2 right-2 hover:border-red-400 hover:bg-red-300">
+        <button
+          className={`absolute px-2 py-1 transition-all bg-gray-300 bg-opacity-90 border-2 border-gray-500 border-solid rounded top-2 right-2 hover:border-red-400 hover:bg-red-300 ${
+            isLiked && 'bg-red-500 border-red-500'
+          }`}
+          onClick={handleClick}
+        >
           <span role="img" aria-label="LIKE BUTTON">
             👍
           </span>
